7293b83a71d2dd2ac538746cb934ea237dae9692
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-09-02 04:20:17 +0100

    fix: update author name and repository URL in package.json; enhance AdapterOptions type definition and improve compileApplication function

--- packages/sveltekit/src/types/AdapterOptions.d.ts
+++ packages/sveltekit/src/types/AdapterOptions.d.ts
@@ -1,21 +1,23 @@
 type Target =
-	| "linux-x64"
-	| "macos-arm64"
-	| "windows-x64"
-	| "darwin-x64"
-	| "darwin-arm64"
-	| "linux-x64-musl"
-	| "linux-arm64-musl";
+  | "linux-x64"
+  | "macos-arm64"
+  | "windows-x64"
+  | "darwin-x64"
+  | "darwin-arm64"
+  | "linux-x64-musl"
+  | "linux-arm64-musl";
 
 export declare type AdapterOptions = {
-	/** Output directory for the built binary (/dist by default) */
-	out?: string;
-	/** Name of the executable binary (default: "app") */
-	binaryName?: string;
-	/** Whether to embed static assets in the binary (default: true) */
-	embedStatic?: boolean;
-	/** Target platform for the binary. By default, the binary is built for the current platform. */
-	target?: Target;
-	/** Volume mount point for the binary (default no volume mount). Can be used for persistent storage, usually /data. */
-	volume?: string;
+  /** Output directory for the built binary (/dist by default) */
+  out?: string;
+  /** Name of the executable binary (default: "app") */
+  binaryName?: string;
+  /** Whether to embed static assets in the binary (default: true) */
+  embedStatic?: boolean;
+  /** Target platform for the binary. By default, the binary is built for the current platform. */
+  target?: Target;
+  /** Volume mount point for the binary (default no volume mount). Can be used for persistent storage, usually /data. */
+  volume?: string;
+  /** Custom port for the server (default: 3000) */
+  port?: number;
 };
