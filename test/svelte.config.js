7636ce9824a51243e589300ff017f753826b9436
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-09-11 04:03:04 +0100

    Refactor code structure for improved readability and maintainability

--- test/svelte.config.js
+++ test/svelte.config.js
@@ -12,7 +12,18 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			binaryName: 'test-example'
+			binaryName: 'test-example',
+			windows: {
+				hideConsole: false,
+				iconPath: './static/app-icon.png', // Make sure this file exists!
+				meta: {
+					title: 'My Awesome SvelteKit App',
+					publisher: 'Your Company Name',
+					version: '1.2.3',
+					description: 'A test application built with SvelteKit Exec Adapter',
+					copyright: '© 2025 Your Company. All rights reserved.'
+				}
+			}
 		})
 	}
 };
